0edb2d9086ed5b62f943cebb71129f2032f6e661
ewiggom-pixel <epf21@georgetown.edu>
2026-07-30 15:45:30 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        r = 0
+
+        checker = defaultdict(int)
+
+        for char in s1:
+            checker[char] += 1
+        
+        for r in range(len(s2)):
+            char = s2[r]
+            checker[char] -= 1
+            
+            while checker[char] < 0:
+                checker[s2[l]] += 1
+                l += 1
+                
+            if (r - l + 1) == len(s1):
+                return True
+        return False
