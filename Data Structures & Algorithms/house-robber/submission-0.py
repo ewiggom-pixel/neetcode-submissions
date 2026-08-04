3897c234ad0168835e1950cabf02c33f0a8d8363
ewiggom-pixel <epf21@georgetown.edu>
2026-08-04 14:34:56 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int], cache = None) -> int:
+        
+        #you have two choice here either rob or skip for each house so to make the change you look at the proior house, but the change that we now need to store the result of robbing or not robbing
+        
+        
+        n  = len(nums)
+        memo = [-1] * n
+
+        def recursive(i):
+
+            if i < 0:
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+
+            memo[i] = max(recursive(i-1), recursive(i-2) + nums[i])
+            return memo[i]
+        return recursive(n-1)
+        
+
