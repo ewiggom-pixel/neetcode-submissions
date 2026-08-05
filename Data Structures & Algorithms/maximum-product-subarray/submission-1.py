da79027e9e8ba5e3989b4db318a052fc022a63ad
ewiggom-pixel <epf21@georgetown.edu>
2026-08-05 09:17:32 -0400

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+
+        # idea is that this dp but we either inlcude or not include the answer as we go throigh it. I also think that two pointers would work here for O(N): Yes it Kadane Algo it looks like but how do we do it with negative bc a negative times a negatative is positive. If we keep a min then we know if a n evil negative will fuck with the result
+
+        result = nums[0]
+        min_pro = 1 
+        max_pro = 1
+        for num in nums:
+            temp = max_pro * num
+            max_pro = max(temp, num* min_pro, num)
+            min_pro = min(temp, num * min_pro, num)
+            result = max(result, max_pro)
+        return result
+        
+
+        
