714b778b44c767b6eb0d8f84a30de581ba2cbcdc
ewiggom-pixel <epf21@georgetown.edu>
2026-08-07 12:42:59 -0400

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        
+        intervals.sort(key = lambda intervals :intervals.start)
+
+        minheap = []
+
+        for interval in intervals:
+            if minheap != [] and interval.start >= minheap[0]:
+                heapq.heappop(minheap)
+            heapq.heappush(minheap, interval.end)
+
+        return len(minheap)
+
+
