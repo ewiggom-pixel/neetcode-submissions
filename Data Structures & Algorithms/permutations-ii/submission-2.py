55bdb0d4ef387c7277c18719856be5adb2d0a294
ewiggom-pixel <epf21@georgetown.edu>
2026-08-11 10:47:27 -0400

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+       
+        perms = [[]]
+        for n in nums:
+            nexPerm = []
+            for p in perms:
+                for i in range(len(p) + 1):
+                    copy = p.copy()
+                    copy.insert(i,n)
+                    if copy not in nexPerm:
+                        nexPerm.append(copy)
+            perms = nexPerm
+        return perms
