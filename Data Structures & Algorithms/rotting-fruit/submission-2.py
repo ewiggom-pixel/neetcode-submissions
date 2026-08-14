32055150fb67c1bd6aa1c191c8d355fb1ac8a642
ewiggom-pixel <epf21@georgetown.edu>
2026-08-14 10:50:41 -0400

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid) , len(grid[0])
+        queue = deque()
+        visit = set()
+        fresh_count = 0
+
+        def bfs(r,c):
+
+            if min(r,c) < 0 or (r,c) in visit or r == rows or c == cols or grid[r][c] == 0:
+                return
+            queue.append([r,c])
+            visit.add((r,c))
+            if grid[r][c] == 1:
+                grid[r][c] = 2
+                nonlocal fresh_count
+                fresh_count -= 1
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 2:
+                    queue.append([i,j])
+                    visit.add((i,j))
+                elif grid[i][j] == 1:
+                    fresh_count += 1
+        
+        if fresh_count == 0: return 0
+        distance = -1
+        while queue:
+            for i in range(len(queue)):
+                r,c = queue.popleft()
+
+                bfs(r+1,c)
+                bfs(r-1,c)
+                bfs(r,c+1)
+                bfs(r,c-1)
+            distance += 1
+        if not any(1 in stuff for stuff in grid):
+            return distance  
+        else:
+            return -1
+
